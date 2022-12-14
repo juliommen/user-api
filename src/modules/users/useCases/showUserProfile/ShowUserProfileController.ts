fe484ac2021351b0e4103fbdd3bb19807aa84466
Júlio Mendonça <juliommen@gmail.com>
2022-12-14 17:38:06 -0400

    show user profile done

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,12 +1,20 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  handle(request, response): Response {
+    const user_id = request.params;
+    let user;
+    try {
+      user = this.showUserProfileUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+    return response.json(user);
   }
 }
 
