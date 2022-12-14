c8521c0486efdf095e8e27f1ec3870b5c65c141f
Júlio Mendonça <juliommen@gmail.com>
2022-12-14 17:30:57 -0400

    turn user admin done

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,12 +1,19 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  handle(request, response): Response {
+    const user_id = request.params;
+    try {
+      this.turnUserAdminUseCase.execute({ user_id });
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+    return response.status(201).send();
   }
 }
 
